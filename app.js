cb3a8d6b475178f877674a997a001fd9ee395e31
Sajjad Ali <71074441+SajjadAliChughtai@users.noreply.github.com>
2023-05-15 02:12:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ var WIIPapp = (function (global) {
         tl.fromTo('.logo-red', 0.25, {scale: 0}, {scale: 1, rotation: 0.01, ease: Power1.easeOut});
         tl.set('.animation', {opacity: 0, ease: Power0.easeNone}, '+=0.5');
         tl.to('.logo-white-xl', 0.5, {css: {top: '50px', left: '10vw', transform: 'scale(0.44)'}, rotation: 0.01, ease: Power0.easeNone});
-        tl.to('.logo-white-l', 0.5, {css: {top: '50px', left: '10vw', transform: 'scale(0.44)'}, rotation: 0.01, ease: Power0.easeNone});
-        tl.to('.logo-white-m', 0.5, {css: {top: '50px', left: '10vw', transform: 'scale(0.44)'}, rotation: 0.01, ease: Power0.easeNone});
-        tl.to('.logo-white-s', 0.5, {css: {top: '50px', left: '10vw', transform: 'scale(0.44)'}, rotation: 0.01, ease: Power0.easeNone});
-        
-        
         tl.set('body', {css: {overflow:'auto'}});
         tl.to('.site-copy', 0.5, {opacity: 1, ease: Power0.easeNone});
         tl.set('.animation', {css: {display:'none'}});
